c67640a6ad697559188724af72a8a25d3d8906d3
Jaroslaw Adamczyk <jaradam1992@gmail.com>
2025-08-15 12:52:03 +0200

    Move middleware to src folder

--- web/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-import { auth } from "@web/auth"
-
-export default auth(async (req) => {
-    console.log('Middleware - req:', req)
-    return NextResponse.next()
-})
